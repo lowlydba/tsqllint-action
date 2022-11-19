6d65a3e11449fcc63d6f6ed772d8805d0a3309e0
John McCall <john@lowlydba.com>
2022-11-19 11:11:20 -0500

    fix no table if pass

--- main.ps1
+++ main.ps1
@@ -62,38 +62,43 @@ $tsqllint_rc = $LASTEXITCODE
 
 # Results
 Get-Content -Path .tsqllint-output
-$errorList = Get-Content -Path .tsqllint-output | Select-Object -Skip 1 | Select-Object -First ($x.Count - 6)
+
 $fullSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 4 | ForEach-Object { $_ + "`n" }
 $warningSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 1
 $errorSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 2 | Select-Object -First 1
 $numWarnings = $warningSummary.Split(" ")[0]
 $numErrors = $errorSummary.Split(" ")[0]
 $summary = $fullSummary
-$table = "| Type | Rule | Location | Message |" + "`n"
-$table += "| ---- | ---- | -------- | ------- |" + "`n"
-foreach ($line in $errorList) {
-    $tableArray = $line.Split(":")
-    $location = $tableArray[0]
-    $err = ($tableArray[1].Split())[1]
-    $rule = ($tableArray[1].Split())[2]
-    $msg = $tableArray[2]
-    $table += "| $err | $rule | $location | $msg |" + "`n"
-}
+if ($numErrors -gt 0 -or $numWarnings -gt 0) {
 
-if ($numErrors -gt 0) {
-    $statusIcon = ":x:"
-}
-elseif ($numWarnings -gt 0) {
-    $statusIcon = ":warning:"
-}
+    $errorList = Get-Content -Path .tsqllint-output | Select-Object -Skip 1 | Select-Object -First ($x.Count - 6)
+    $table = "| Type | Rule | Location | Message |" + "`n"
+    $table += "| ---- | ---- | -------- | ------- |" + "`n"
+    foreach ($line in $errorList) {
+        $tableArray = $line.Split(":")
+        $location = $tableArray[0]
+        $err = ($tableArray[1].Split())[1]
+        $rule = ($tableArray[1].Split())[2]
+        $msg = $tableArray[2]
+        $table += "| $err | $rule | $location | $msg |" + "`n"
+    }
 
+    if ($numErrors -gt 0) {
+        $statusIcon = ":x:"
+    }
+    elseif ($numWarnings -gt 0) {
+        $statusIcon = ":warning:"
+    }
+}
 # Build comment
 "## $statusIcon TSQLLint Summary" | Out-File $commentFile
 "`n$summary" | Out-File $commentFile -Append
-"`n<details>" | Out-File $commentFile -Append
-"`n<summary>See results</summary>" | Out-File $commentFile -Append
-"`n$table" | Out-File $commentFile -Append
-"`n</details>" | Out-File $commentFile -Append
+if ($table) {
+    "`n<details>" | Out-File $commentFile -Append
+    "`n<summary>See results</summary>" | Out-File $commentFile -Append
+    "`n$table" | Out-File $commentFile -Append
+    "`n</details>" | Out-File $commentFile -Append
+}
 "`n[Detailed results.]($env:GITHUB_SERVER_URL/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID)" | Out-File $commentFile -Append
 "`n:recycle: This comment has been updated with latest results." | Out-File $commentFile -Append
 
