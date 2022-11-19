45ca05e4951e395ec9eae9e60da73af1c3a8bba0
John McCall <john@lowlydba.com>
2022-11-19 14:46:20 -0500

    add comment table of results (#10)
    * add comment table of results
    
    * fix no table if pass
    
    * test fails
    
    * Update test.yml
    
    * Update main.ps1
    
    * Update test.yml
    
    * Update main.ps1
    
    * Update main.ps1
    
    * Update main.ps1
    
    * Update main.ps1
    
    * re-add pass tests
    
    * Update main.ps1
    
    * Update main.ps1

--- main.ps1
+++ main.ps1
@@ -9,12 +9,15 @@ $commentFile = Join-Path -Path $env:RUNNER_TEMP -ChildPath ".tsqllint-output.fin
 "COMMENT_FILE=$commentFile" >> $env:GITHUB_ENV
 $statusIcon = ":white_check_mark:"
 $baseCommand = "tsqllint"
+$configCommand = $baseCommand + " --init"
 if ($Config) {
-    $baseCommand = $baseCommand + " -c $Config"
+    if (Test-Path -Path $Config) {
+        $baseCommand = $baseCommand + " -c $Config"
+        $configCommand = $baseCommand + " -p"
+    }
 }
-$configCommand = $baseCommand + " -p"
-$versionCommand = "tsqllint -v"
 
+$versionCommand = "tsqllint -v"
 # Show config
 $ConfigSetting = Invoke-Expression -Command $configCommand
 $ConfigSetting = $ConfigSetting | Select-Object -Last 1
@@ -39,14 +42,14 @@ if ($OnlyChangedFiles -eq "true" -and $env:GITHUB_HEAD_REF) {
 }
 
 # Lint
-if ($files -eq $null) {
+if ($null -eq $files) {
     Write-Host "No modified or added files detected for linting."
     "tsqllint_skip_comment=true" >> $env:GITHUB_ENV
     exit 0
 }
 
 try {
-    if ($Config) {
+    if (Test-Path -Path $Config) {
         tsqllint $files -c $Config | Out-File .tsqllint-output
     }
     else {
@@ -62,25 +65,43 @@ $tsqllint_rc = $LASTEXITCODE
 
 # Results
 Get-Content -Path .tsqllint-output
+
 $fullSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 4 | ForEach-Object { $_ + "`n" }
 $warningSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 1
 $errorSummary = Get-Content -Path .tsqllint-output | Select-Object -Last 2 | Select-Object -First 1
 $numWarnings = $warningSummary.Split(" ")[0]
 $numErrors = $errorSummary.Split(" ")[0]
-
-if ($numErrors -gt 0) {
-    $statusIcon = ":x:"
-}
-elseif ($numWarnings -gt 0) {
-    $statusIcon = ":warning:"
-}
-
 $summary = $fullSummary
+if ($numErrors -gt 0 -or $numWarnings -gt 0) {
+    $errorList = Get-Content -Path .tsqllint-output | Select-Object -Skip 1 | Select-Object -First ((Get-Content -Path .tsqllint-output).Count - 6)
+    $table = "| Type | Rule | Location | Message |" + "`n"
+    $table += "| ---- | ---- | -------- | ------- |" + "`n"
+    foreach ($line in $errorList) {
+        $tableArray = $line.Split(":")
+        $location = $tableArray[0]
+        $err = ($tableArray[1].Split())[1]
+        $rule = ($tableArray[1].Split())[2]
+        $msg = $tableArray[2]
+        $table += "| $err | [$rule](https://github.com/tsqllint/tsqllint/blob/main/documentation/rules/$rule.md) | $location | $msg |" + "`n"
+    }
 
+    if ($numErrors -gt 0) {
+        $statusIcon = ":x:"
+    }
+    elseif ($numWarnings -gt 0) {
+        $statusIcon = ":warning:"
+    }
+}
 # Build comment
 "## $statusIcon TSQLLint Summary" | Out-File $commentFile
 "`n$summary" | Out-File $commentFile -Append
-"`n[Detailed results.]($env:GITHUB_SERVER_URL/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID)" | Out-File $commentFile -Append
+if ($table) {
+    "`n<details>" | Out-File $commentFile -Append
+    "`n<summary>See results</summary>" | Out-File $commentFile -Append
+    "`n$table" | Out-File $commentFile -Append
+    "`n</details>" | Out-File $commentFile -Append
+}
+"`n:page_facing_up: Full [job results]($env:GITHUB_SERVER_URL/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID)." | Out-File $commentFile -Append
 "`n:recycle: This comment has been updated with latest results." | Out-File $commentFile -Append
 
 exit 0
